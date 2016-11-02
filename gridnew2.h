ca4f8adc567fef9123277d190467b0a7abd94649
rishabhchitlangia <rishabhgr8.chitlangia@gmail.com>
2016-11-02 08:55:43 +0000

    Update gridnew2.h

--- gridnew2.h
+++ gridnew2.h
@@ -16,15 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
-***********************************************************************************
-
-
-
-
-
-
-
-
+**********************************************************************************
 
 typedef enum {VALID, SPACE_OCCUPIED, OUT_OF_BOUNDS} MoveStatus;
 int big[3][3];
